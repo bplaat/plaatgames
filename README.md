87303ee37bf6690983f2d73bc2740054e41625bf
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-08-19 10:58:43 +0200

    Update this old site so that it works again

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# plaatgames
-A dutch game site filled with cool local games to play
-when your bored at school :)
+# PlaatGames
+
+A dutch game site filled with cool local games to play when your bored at school :)
--- plaatgames.js
+++ plaatgames.js
@@ -1,49 +1,38 @@
-var game = document.getElementById('game');
-if (game) {
-    function resize () {
-        if (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
-            if (window.innerWidth < game.offsetWidth * window.innerHeight / game.offsetHeight) {
-                game.style.transform = 'translateY(' + (window.innerHeight - game.offsetHeight *
-                    (window.innerWidth / game.offsetWidth)) / 2 + 'px) scale(' + window.innerWidth / game.offsetWidth + ')';
+window.addEventListener('load', () => {
+    const game = document.getElementById('game');
+    if (game) {
+        function resize () {
+            if (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+                if (window.innerWidth < game.offsetWidth * window.innerHeight / game.offsetHeight) {
+                    game.style.transform = 'scale(' + window.innerWidth / game.offsetWidth + ')';
+                } else {
+                    game.style.transform = 'scale(' + window.innerHeight / game.offsetHeight + ')';
+                }
             } else {
-                game.style.transform = 'translateX(' + (window.innerWidth - game.offsetWidth *
-                (window.innerHeight / game.offsetHeight)) / 2 + 'px) scale(' + window.innerHeight / game.offsetHeight + ')';
-            }
-        } else {
-            if ((window.innerWidth - 32) < game.offsetWidth * (window.innerHeight - 88) / game.offsetHeight) {
-                game.style.transform = 'translateY(' + ((window.innerHeight - 88) - game.offsetHeight *
-                    ((window.innerWidth - 32) / game.offsetWidth)) / 2 + 'px) scale(' + (window.innerWidth - 32) / game.offsetWidth + ')';
-            } else {
-                game.style.transform = 'translateX(' + ((window.innerWidth - 32) - game.offsetWidth *
-                ((window.innerHeight - 88) / game.offsetHeight)) / 2 + 'px) scale(' + (window.innerHeight - 88) / game.offsetHeight + ')';
+                if ((window.innerWidth - 32) < game.offsetWidth * (window.innerHeight - 88) / game.offsetHeight) {
+                    game.style.transform = 'scale(' + (window.innerWidth - 32) / game.offsetWidth + ')';
+                } else {
+                    game.style.transform = 'scale(' + (window.innerHeight - 88) / game.offsetHeight + ')';
+                }
             }
         }
+        resize();
+        window.addEventListener('resize', resize);
+
+        const wrapper = document.getElementById('wrapper');
+        const fullscreen = document.getElementById('fullscreen');
+        fullscreen.addEventListener('click', function (event) {
+            event.preventDefault();
+            this.blur();
+            if (wrapper.requestFullscreen) {
+                wrapper.requestFullscreen();
+            } else if (wrapper.mozRequestFullScreen) {
+                wrapper.mozRequestFullScreen();
+            } else if (wrapper.webkitRequestFullscreen) {
+                wrapper.webkitRequestFullscreen();
+            } else if (wrapper.msRequestFullscreen) {
+                wrapper.msRequestFullscreen();
+            }
+        });
     }
-    resize();
-    window.addEventListener('resize', resize);
-
-    var wrapper = document.getElementById('wrapper');
-    var fullscreen = document.getElementById('fullscreen');
-    fullscreen.addEventListener('click', function (event) {
-        event.preventDefault();
-        this.blur();
-        if (wrapper.requestFullscreen) {
-            wrapper.requestFullscreen();
-        } else if (wrapper.mozRequestFullScreen) {
-            wrapper.mozRequestFullScreen();
-        } else if (wrapper.webkitRequestFullscreen) {
-            wrapper.webkitRequestFullscreen();
-        } else if (wrapper.msRequestFullscreen) {
-            wrapper.msRequestFullscreen();
-        }
-    });
-}
-
-var analyticsScript = document.createElement('script');
-analyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=UA-110826300-1';
-document.head.appendChild(analyticsScript);
-
-window.dataLayer = window.dataLayer || [];
-function gtag () { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-110826300-1');
+});
